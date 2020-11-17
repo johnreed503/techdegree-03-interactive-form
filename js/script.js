ce9aa0095a77f75f8d37bc4455972b90790feaab
John Reed <contact.johnreed@gmail.com>
2020-11-16 21:57:14 -0500

    updated checkboxes to disable for competing times. Having trouble showing the total cost

--- js/script.js
+++ js/script.js
@@ -31,35 +31,43 @@ designTheme.addEventListener('change', (event) => {
   }
 })
 
+const checkboxArea = document.querySelector('.activities')
+//enable and disable checkboxes
+const checkboxes = document.querySelectorAll('.activities, input');
 
+document.querySelector('.activities').addEventListener('change', e => {
+  let clicked = e.target
+  let clickedType = e.target.getAttribute('data-day-and-time')
+  console.log(clicked, clickedType)
+  for ( let i = 0; i < checkboxes.length; i++ ) {
+    let checkboxType = checkboxes[i].getAttribute('data-day-and-time')
+    console.log(clickedType)
+    if (clickedType !== null) {
+    if ( clickedType === checkboxType && clicked !== checkboxes[i]) {
+       if (clicked.checked === true) {
+         checkboxes[i].disabled = true
+       } else{
+         checkboxes[i].disabled = false
+       }
+     }
 
+   }
 
+  }
+  let totalCost = 0
+  for ( let i = 0; i < checkboxes.length; i++ ) {
+    if (checkboxes[i].checked === true) {
+    let value = parseInt(e.target.getAttribute('data-cost'), 10)
+    totalCost += value
 
-// designTheme.addEventListener('change', (event) => {
-//
-//   if (designTheme.value === 'js puns') {
-//     colorSelector.innerHTML = `
-//       <option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
-//       <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>
-//       <option value="gold">Gold (JS Puns shirt only)</option>
-//       `
-//   } else if (designTheme.value === 'heart js') {
-//     colorSelector.innerHTML = `
-//       option value="tomato">Tomato (I &#9829; JS shirt only)</option>
-//       <option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option>
-//       <option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>
-//       `
-//   } else {
-//     colorSelector.innerHTML = `
-//       <option value="select theme">Please select a T-shirt theme</option>
-//     `
-//   }
-//
-//   // else (designTheme.value === 'Theme - JS Puns') {
-//   //   colorSelector.innerHTML = `<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
-//   //   <option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option>
-//   //   <option value="gold">Gold (JS Puns shirt only)</option>`
-//   // }
-//
-//
-// })
+    html = `
+      <span>$${totalCost}</span>
+      `
+    checkboxArea.insertAdjacentHTML('beforeend', html);
+  }
+  }
+  // html = `
+  //   <span>$${totalCost}</span>
+  //   `
+  // checkboxArea.insertAdjacentHTML('beforeend', html);
+});
